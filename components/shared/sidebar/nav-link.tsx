eb6b442a792932b00de02d57905a4ae9f16a21a9
wira <wirayuda0979@gmail.com>
2023-12-25 18:16:13 +0800

    remove clg

--- components/shared/sidebar/nav-link.tsx
+++ components/shared/sidebar/nav-link.tsx
@@ -1,70 +1,69 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import SearchForm from './search-form';
-import { Thread } from '@/types/threads';
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SearchForm from "./search-form";
+import { Thread } from "@/types/threads";
 
 type NavLinkProps = {
-	route: string;
-	label: string;
-	imgURL: string;
+  route: string;
+  label: string;
+  imgURL: string;
 };
 
 export default function NavLink({ route, label, imgURL }: NavLinkProps) {
-	const pathname = usePathname();
-	const [result, setResult] = useState<Thread[]>([]);
+  const pathname = usePathname();
+  const [result, setResult] = useState<Thread[]>([]);
 
-  console.log("Hello")
-	return (
-		<>
-			{route === '/search' ? (
-				<Sheet>
-					<SheetTrigger
-						className={`ease inline-flex items-center gap-3 rounded-full p-2 !text-white transition-all duration-300 hover:bg-primary-500 md:w-full  md:rounded-lg lg:p-3 ${
-							pathname === route ? 'bg-primary-500' : ''
-						}`}
-					>
-						<Image
-							className='h-5 w-5 object-contain'
-							src={imgURL}
-							width={30}
-							height={30}
-							alt={label}
-						/>
-						<span className=' hidden text-white md:!block'>{label}</span>
-					</SheetTrigger>
+  return (
+    <>
+      {route === "/search" ? (
+        <Sheet>
+          <SheetTrigger
+            className={`ease inline-flex items-center gap-3 rounded-full p-2 !text-white transition-all duration-300 hover:bg-primary-500 md:w-full  md:rounded-lg lg:p-3 ${
+              pathname === route ? "bg-primary-500" : ""
+            }`}
+          >
+            <Image
+              className="h-5 w-5 object-contain"
+              src={imgURL}
+              width={30}
+              height={30}
+              alt={label}
+            />
+            <span className=" hidden text-white md:!block">{label}</span>
+          </SheetTrigger>
 
-					<SheetContent
-						onBlur={() => setResult([])}
-						side='left'
-						className='h-screen overflow-y-auto'
-					>
-						<SearchForm result={result} setResult={setResult} />
-					</SheetContent>
-				</Sheet>
-			) : (
-				<Link
-					href={route}
-					className={`ease inline-flex items-center gap-3 rounded-full p-2 !text-white transition-all  duration-300 hover:bg-primary-500 md:w-full md:rounded-lg lg:p-3 ${
-						pathname === route ? 'bg-primary-500' : ''
-					}`}
-					key={label}
-				>
-					<Image
-						className='h-5 w-5 object-contain'
-						src={imgURL}
-						width={30}
-						height={30}
-						alt={label}
-					/>
-					<span className=' hidden text-white md:!block'>{label}</span>
-				</Link>
-			)}
-		</>
-	);
+          <SheetContent
+            onBlur={() => setResult([])}
+            side="left"
+            className="h-screen overflow-y-auto"
+          >
+            <SearchForm result={result} setResult={setResult} />
+          </SheetContent>
+        </Sheet>
+      ) : (
+        <Link
+          href={route}
+          className={`ease inline-flex items-center gap-3 rounded-full p-2 !text-white transition-all  duration-300 hover:bg-primary-500 md:w-full md:rounded-lg lg:p-3 ${
+            pathname === route ? "bg-primary-500" : ""
+          }`}
+          key={label}
+        >
+          <Image
+            className="h-5 w-5 object-contain"
+            src={imgURL}
+            width={30}
+            height={30}
+            alt={label}
+          />
+          <span className=" hidden text-white md:!block">{label}</span>
+        </Link>
+      )}
+    </>
+  );
 }
