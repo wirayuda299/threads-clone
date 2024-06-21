ef68ff8349ac4a63d77a1bb7c78a7748a669ab6e
wirayuda <81408241+wirayuda299@users.noreply.github.com>
2024-06-21 12:04:22 +0800

    Update Header.tsx

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 
 import { UserButton } from "@clerk/nextjs";
@@ -10,13 +9,7 @@ export default function Header() {
     <header className="fixed top-0 z-30 flex h-20 w-full items-center justify-between bg-main p-5 text-white">
       <div>
         <Link className="flex items-center gap-3" href="/">
-          <Image
-            src="/assets/logo.png"
-            className="h-7 w-7 object-contain"
-            width={35}
-            height={35}
-            alt="Threads"
-          />
+
           <h1 className="text-2xl font-semibold">Threads</h1>
         </Link>
       </div>
